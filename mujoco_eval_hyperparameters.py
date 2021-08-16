3badc2ab2651708280b6433e2e77dcc8e556683a
SonSang <shh1295@gmail.com>
2021-08-16 09:19:05 -0400

    Changed evaluation code

--- mujoco_eval_hyperparameters.py
+++ mujoco_eval_hyperparameters.py
@@ -15,7 +15,9 @@ from typing import Dict, Any
 
 n_evaluations = 20
 n_envs = 1
-n_timesteps = 1e6
+n_timesteps = 1e7
+n_params = 10
+n_test_runs = 10
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--study-name", help="Study name used during hyperparameter optimization", type=str, default=None)
@@ -23,7 +25,7 @@ parser.add_argument("--env-name", help="Env to use during hyperaparameter evalua
 args = parser.parse_args()
 
 study_dir = './mujoco_hyperparameters/' + args.study_name
-for param_id in range(2):
+for param_id in range(n_params):
     param_file = study_dir + "/hyperparameters_" + str(param_id) + ".json"
     with open(param_file) as f:
         params = json.load(f)
@@ -68,7 +70,7 @@ for param_id in range(2):
 
     eval_log_file = eval_log_dir + 'reward_stat.txt'
     with open(eval_log_file, "w+") as f:
-        for i in range(10):
+        for i in range(n_test_runs):
             model = PPO('MlpPolicy', env, verbose=0, **params)
             eval_callback = EvalCallback(eval_env, best_model_save_path=eval_log_dir, log_path=eval_log_dir, eval_freq=eval_freq, deterministic=True, render=False)
             model.learn(total_timesteps=n_timesteps, callback=eval_callback)
@@ -131,40 +133,3 @@ for _ in range(video_length + 1):
     if done:
         break
 env.close()
-
-# env = pistonball_v4.parallel_env()
-# env = ss.color_reduction_v0(env, mode='B')
-# env = ss.resize_v0(env, x_size=84, y_size=84)
-# env = ss.frame_stack_v1(env, 3)
-# env = ss.pettingzoo_env_to_vec_env_v0(env)
-# env = ss.concat_vec_envs_v0(env, n_envs, num_cpus=1, base_class='stable_baselines3')
-# env = VecMonitor(env)
-# env = image_transpose(env)
-
-# eval_env = pistonball_v4.parallel_env()
-# eval_env = ss.color_reduction_v0(eval_env, mode='B')
-# eval_env = ss.resize_v0(eval_env, x_size=84, y_size=84)
-# eval_env = ss.frame_stack_v1(eval_env, 3)
-# eval_env = ss.pettingzoo_env_to_vec_env_v0(eval_env)
-# eval_env = ss.concat_vec_envs_v0(eval_env, 1, num_cpus=1, base_class='stable_baselines3')
-# eval_env = VecMonitor(eval_env)
-# eval_env = image_transpose(eval_env)
-
-# eval_freq = int(n_timesteps / n_evaluations)
-# eval_freq = max(eval_freq // (n_envs * n_agents), 1)
-
-# all_mean_rewards = []
-# for i in range(10):
-#     model = PPO("CnnPolicy", env, verbose=3, **params)
-#     eval_callback = EvalCallback(eval_env, best_model_save_path='./eval_logs/' + num + '/', log_path='./eval_logs/' + num + '/' , eval_freq=eval_freq, deterministic=True, render=False)
-#     model.learn(total_timesteps=n_timesteps, callback=eval_callback) 
-#     model = PPO.load('./eval_logs/' + num + '/' + 'best_model')
-#     mean_reward, std_reward = evaluate_policy(model, eval_env, deterministic=True, n_eval_episodes=25)
-#     print(mean_reward)
-#     print(std_reward)
-#     all_mean_rewards.append(mean_reward)
-#     if mean_reward > 90:
-#         model.save('./mature_policies/' + str(num) + '/' + str(i) + '/')
-
-
-# print(sum(all_mean_rewards) / len(all_mean_rewards))
