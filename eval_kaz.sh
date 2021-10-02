112ee2b536b400a9c8213c1cfd1f0a3b83c7dbfa
SonSang <shh1295@gmail.com>
2021-10-02 03:41:14 -0400

    added cuda and thread option

--- eval_kaz.sh
+++ eval_kaz.sh
@@ -1,1 +1,1 @@
-python indicator_eval_params.py --study-name kaz_1_1e7_0 --env-name knights-archers-zombies-v7 --parameter-id 0 --n-runs 10 --timesteps 10000000
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=1 python indicator_eval_params.py --study-name kaz_1_1e7_0 --env-name knights-archers-zombies-v7 --parameter-id 0 --n-runs 10 --timesteps 10000000
