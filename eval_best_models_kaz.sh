7817a116c26dd0b4253fb6eeb513650768f6e1bc
SonSang <shh1295@gmail.com>
2021-10-20 03:16:56 -0400

    add KAZ evaluation result

--- eval_best_models_kaz.sh
+++ eval_best_models_kaz.sh
@@ -1,8 +1,8 @@
-python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 0 --n-runs 10
-python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 1 --n-runs 10
-python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 2 --n-runs 10
-python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 3 --n-runs 10
-python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 4 --n-runs 10
+#python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 0 --n-runs 10
+#python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 1 --n-runs 10
+#python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 2 --n-runs 10
+#python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 3 --n-runs 10
+#python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 4 --n-runs 10
 python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 5 --n-runs 10
 python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 6 --n-runs 10
 python indicator_evaluate_best_models.py --env-name knights-archers-zombies-v7 --study-name kaz_1_1e7_0 --parameter-id 7 --n-runs 10
